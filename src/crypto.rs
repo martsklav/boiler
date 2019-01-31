33c09867a7f9cd2e1438b66dd3ea882b55e0f6af
martsklav <39799175+martsklav@users.noreply.github.com>
2019-01-30 19:06:17 -0500

    Update crypto.rs

--- src/crypto.rs
+++ src/crypto.rs
@@ -37,7 +37,8 @@ fn crypt_iv(iv: &[u8], key: &[u8], mode: Mode) -> Vec<u8> {
     let crypter = Crypter::new(Cipher::aes_256_ecb(), mode, key, enum_primitive::Option::Some("".as_bytes())).unwrap();
 
     // Actually perform the encryption
-    let mut buffer = crypter.update(&iv);
+    let mut buffer;
+    crypter.update(&iv, buffer);
     buffer.extend_from_slice(&crypter.finalize());
 
     buffer
